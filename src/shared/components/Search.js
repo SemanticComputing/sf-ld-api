25b441e687d500abd683f3ae4076f69d4cd08daf
Erkki Heino <evsheino@gmail.com>
2017-09-04 15:33:30 +0300

    Fix text search submit using the enter key, and add debounce

--- src/shared/components/Search.js
+++ src/shared/components/Search.js
@@ -1,6 +1,6 @@
 import React                                            from 'react';
 import { Button, ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
-import _                                                from 'lodash';
+import { map, debounce }                                from 'lodash';
 import Promise                                          from 'bluebird';
 import Autocomplete                                     from 'react-autocomplete';
 import statuteCtrl                                      from '../ctrl/statuteCtrl';
@@ -9,11 +9,12 @@ import SearchResult                                     from './SearchResult';
 
 export default class Search extends React.Component {
 
-
   constructor(props) {
     super(props);
-    const data = this.props.route.data;
-    this.handleQueryChange = this.handleQueryChange.bind(this);
+
+    this.delayedQueryChanged = debounce(this.handleQueryChange.bind(this), 200);
+    this.onQueryChange = this.onQueryChange.bind(this);
+
     this.handleDocCategoryChange = this.handleDocCategoryChange.bind(this);
     this.state = {
       query: '',
@@ -26,7 +27,6 @@ export default class Search extends React.Component {
     };
   }
 
-
   getQueryHandler(docCategory = this.state.docCategory) {
     const handlers = {
       'sd': statuteCtrl.findByQuery,
@@ -35,43 +35,49 @@ export default class Search extends React.Component {
     return handlers['sd'];
   }
 
-
   handleDocCategoryChange(event) {
     this.setState({
       docCategory: event.target.value
     });
   }
 
-
-  handleQueryChange(event, value) {
+  onQueryChange(event, value) {
+    event.persist();
     const ts = new Date().getTime();
-    this.setState({acQueryTs: new Date(ts).getTime()});
-    this.setState({ value, loading: true, query : value  });
-    this.queryAc(value)
+    this.setState({
+      acQueryTs: ts,
+      value,
+      loading: true,
+      query: value
+    });
+    this.delayedQueryChanged(event, value, ts);
+  }
+
+  handleQueryChange(event, value, ts) {
+    return this.queryAc(value)
       .then((items) => {
-        const itemsMod = _.map(items, (item) => {
+        const itemsMod = map(items, (item) => {
           //if (item.st.value == 'narrower') item.label = item.cl.value+' ↳ '+item.sl.value;
           //else if (item.st.value == 'related') item.label = item.cl.value+' → '+item.sl.value;
           //else
           item.label = item.sl.value;
           return item;
         });
-        console.log(items);
         if (ts == this.state.acQueryTs)
           this.setState({ autoComplete: itemsMod, loading: false });
       })
       .catch((err) => { console.log(err);});
   }
 
-
-  query() {
+  query(event) {
+    event.preventDefault();
     const query = this.state.query;
     const ts = new Date().getTime();
-    this.setState({queryTs: new Date(ts).getTime()});
+    this.setState({queryTs: ts});
     this.getQueryHandler()({query: query})
       .then((results) => {
         if (ts == this.state.queryTs) {
-          const searchResults = _.map(results, (result, idx) => {
+          const searchResults = map(results, (result, idx) => {
             return <SearchResult
               key={idx+'-'+new Date().getTime()}
               title={result.title ? result.title.value : ''}
@@ -92,7 +98,6 @@ export default class Search extends React.Component {
         console.log(err);
       });
   }
-
 
   // Autocomplete
   queryAc(query) {
@@ -115,7 +120,7 @@ export default class Search extends React.Component {
       <div className="search">
         <h1>Haku</h1>
         <div className="search-bar">
-          <form>
+          <form onSubmit={(e) => this.query(e)}>
             <div className="query-doc-category" style={{display: 'inline-block'}}>
               <FormControl value={this.state.docCategory} onChange={this.handleDocCategoryChange} componentClass="select" placeholder="Valitse">
                 <option key={0} value="sd">Lainsäädäntö</option>
@@ -127,14 +132,13 @@ export default class Search extends React.Component {
               placeholder="Nimi"
               wrapperProps={{ className: 'query-wrapper' }}
               inputProps={{ id: 'query-autocomplete', placeholder: 'Hakusana(t)', className: 'form-control' }}
-              ref="autocomplete"
               value={this.state.value}
               items={this.state.autoComplete}
               getItemValue={(item) => item.sl.value}
               onSelect={(value, item) => {
                 this.setState({ value, autoComplete: [item], query : item.sl.value });
               }}
-              onChange={this.handleQueryChange}
+              onChange={this.onQueryChange}
               renderItem={(item, isHighlighted) => (
                 <div
                   style={{ background: isHighlighted ? 'lightgray' : 'white' }}
@@ -143,7 +147,7 @@ export default class Search extends React.Component {
                 >{item.label}</div>
               )}
             />
-            <Button className="query-button" bsStyle="primary" onClick={() => this.query()}>Hae</Button>
+            <Button type="submit" className="query-button" bsStyle="primary">Hae</Button>
           </form>
         </div>
         <div className="search-results">
