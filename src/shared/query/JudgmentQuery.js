9148065a4bb331d6a5308ef8d00ca97364274a16
Erkki Heino <evsheino@gmail.com>
2017-10-06 11:52:43 +0300

    Refactor judgement queries

--- src/shared/query/JudgmentQuery.js
+++ src/shared/query/JudgmentQuery.js
@@ -1,79 +1,56 @@
-import moment from 'moment';
 import sfcl from '../lib/sfcl';
 import common from '../lib/common';
 
-export default class JudgmentQuery {
+const judgmentQuery = {
 
-  constructor(params = {}) {
-    console.log(params);
+  findMany: (params) => {
+    if (!(params.court || params.year)) {
+      throw Error('Either court or year is required');
+    }
+    const lang = common.get2LetterLangCode(params.lang || 'fi');
+    const limit = params.limit ? 'LIMIT ' + params.limit : (params.year) ? '' : 'LIMIT 10';
+    let judgment = '';
+    if (params.court) {
+      judgment += `?judgment dcterms:creator ${common.getCourtByName(params.court)} . `;
+    }
+    if (params.year) {
+      judgment += `?judgment dcterms:date ?date . FILTER(YEAR(?date) = ${parseInt(params.year)})`;
+    }
 
-    this.vars = '*';
-    // Default limit
-    this.limit = params.limit ? 'LIMIT '+params.limit : (params.year) ? '':'LIMIT 10';
-    // Judgments by court
-    this.judgment = (params.court) ? '?judgment a sfcl:Judgment . ?judgment dcterms:creator  '+common.getCourtByName(params.court)+'.' : '?judgment a sfcl:Judgment .';
-    // Judgments by year
-    this.judgment = (params.year) ? '?judgment dcterms:date ?date .FILTER(year(?date) = '+parseInt(params.year)+')' : this.judgment;
-    // Judgment by id
-    this.judgment = (params.judgmentId) ? 'VALUES ?judgment { sfecli:'+params.court+'\\/'+params.year+'\\/'+params.judgmentId+' }' : this.judgment;
-    // Filter by lang
-    this.ecliLangFilter = params.lang ? '?expression dcterms:language "'+common.get2LetterLangCode(params.lang)+'".' : '?expression dcterms:language "fi".';
-    // Filter by format
-    this.content = '?format a sfcl:Format . ?format '+((params.format) ? sfcl.getPropertyByFormat(params.format) : sfcl.getPropertyByFormat('text'))+' ?content .';
-    // Bind judgment to variable j
-    this.judgmentBind = (params.judgmentId) ? 'BIND(sfecli:'+params.court+'\\/'+params.year+'\\/'+params.judgmentId+' AS ?judgment)' : '';
-  }
-
-  findMany() {
-    return `SELECT ${this.vars} WHERE {
-      ${this.judgment}
-      ?judgment a ?judgmentType .
+    return `SELECT * WHERE {
+      ${judgment}
       ?judgment dcterms:isVersionOf ?ecli .
       ?judgment sfcl:isRealizedBy ?expression .
-      ${this.ecliLangFilter}
+      ?expression dcterms:language "${lang}" .
       ?expression dcterms:title ?title .
       ?expression a ?expressionType .
-    } ${this.limit}`;
-  }
+      ?judgment a ?judgmentType .
+    } ${limit}`;
+  },
 
-  findOne() {
-    return `SELECT ${this.vars} WHERE {
+  findOne: (params) => {
+    const judgmentUri = `sfecli:${params.court}\\/${params.year}\\/${params.judgmentId}`;
+    const lang = common.get2LetterLangCode(params.lang || 'fi');
+    const contentProperty = sfcl.getPropertyByFormat(params.format || 'text');
+    return `SELECT * WHERE {
       {
-        ${this.judgment}
+        VALUES ?judgment { ${judgmentUri} }
         ?judgment dcterms:isVersionOf ?ecli .
         ?judgment sfcl:isRealizedBy ?expression .
-        ${this.ecliLangFilter}
+        ?expression dcterms:language "${lang}" .
         ?expression dcterms:title ?title .
         ?expression a ?expressionType .
         ?expression sfcl:isEmbodiedBy ?format .
-        ${this.content}
+        ?format a sfcl:Format .
+        ?format ${contentProperty} ?content .
       }
       UNION {
-       ${this.judgmentBind}
+       BIND(${judgmentUri} AS ?judgment)
        ?judgment ?p ?o.
        FILTER (?p != sfcl:isRealizedBy)
       }
     }`;
   }
+};
 
-  findManyByQuery() {
-    return `SELECT ${this.vars} WHERE {
-      {
-        ${this.judgment}
-        ?judgment dcterms:isVersionOf ?ecli .
-        ?judgment sfcl:isRealizedBy ?expression .
-        ${this.ecliLangFilter}
-        ?expression dcterms:title ?title .
-        ?expression a ?expressionType .
-        ?expression sfcl:isEmbodiedBy ?format .
-        ${this.content}
-      }
-      UNION {
-       ${this.judgmentBind}
-       ?judgment ?p ?o.
-       FILTER (?p != sfcl:isRealizedBy)
-      }
-    }`;
-  }
-
-}
+export default judgmentQuery;
