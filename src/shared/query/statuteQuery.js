924bfeb73a8d6060e14cf04ad3a127035b74f26e
Erkki Heino <evsheino@gmail.com>
2017-10-06 17:15:38 +0300

    Statute text search

--- src/shared/query/statuteQuery.js
+++ src/shared/query/statuteQuery.js
@@ -3,138 +3,9 @@ import eli from '../lib/eli';
 import sfl from '../lib/sfl';
 
 const FREE_GRAPH = '<http://data.finlex.fi/eli/sd/alkup>';
+const DEFAULT_LIMIT = 10;
 
 const statuteQuery = {
-
-  findMany: (params) => {
-    const fromGraph = params.free ? `FROM ${FREE_GRAPH}` : '';
-    const yearFilter = params.year ? `FILTER STRENDS(?idLocal, "${params.year}")` : '';
-    const limit = params.year ? '' : `LIMIT ${params.limit || 10}`;
-    const offset = params.offset ? `OFFSET ${params.offset}` : '';
-    const lang = eli.getLangResource(params.lang || 'fi');
-    const original = params.version === 'alkup' ? '?statuteVersion eli:version sfl:Original .' : '';
-
-    return `SELECT * ${fromGraph} WHERE {
-      {
-        SELECT DISTINCT ?statute ?year ?number ?letter {
-          ${original}
-          ?statute a sfl:Statute .
-          ?statute eli:id_local ?idLocal .
-          ${yearFilter}
-          BIND(xsd:integer(REPLACE(?idLocal, "[^0-9]+[0-9]+", "")) AS ?number)
-          BIND(REPLACE(?idLocal, "[^A-Z]", "") AS ?letter)
-        } ORDER BY ?year ?number ?letter ${limit} ${offset}
-      }
-      FILTER(BOUND(?statute))
-      ?statute eli:has_member ?statuteVersion .
-      ?statute eli:id_local ?idLocal .
-      ?statute sfl:statuteType ?statuteType .
-      ?statuteVersion eli:is_realized_by ?expression .
-      ?statuteVersion a ?statuteVersionType .
-      ?expression eli:language ${lang} .
-      ?expression eli:title ?title .
-      ?expression a ?expressionType .
-    }`;
-  },
-
-  findManyByQuery: (params) => {
-    const query = (params.query) ? params.query.replace(/[^a-zA-ZäöåÄÖÅ0-9*"\s]/gi,'').toLowerCase() : '';
-    const queryBaseForm = (params.queryBaseForm) ? params.queryBaseForm : '';
-    return `
-      SELECT DISTINCT ?c ?l ?v ?s ?st ?stt ?t ?title ?txt ?score ?matchType WHERE {
-       {
-          ?e text:query (eli:title '${query}' 20) .
-          ?s eli:has_member ?v.
-          ?v eli:is_realized_by ?e .
-          ?e eli:is_embodied_by ?f .
-          ?e eli:title ?title .
-          ?e eli:is_embodied_by ?f2 .
-          ?f2 sfl:html ?txt .
-          FILTER NOT EXISTS {
-            ?s eli:in_force eli:InForce-notInForce .
-          }
-          OPTIONAL {
-            ?v eli:is_part_of+ ?st .
-            ?st eli:is_member_of ?stw .
-            ?stw a sfl:Statute .
-            FILTER NOT EXISTS {
-              ?stw eli:in_force eli:InForce-notInForce .
-            }
-            ?st eli:is_realized_by ?ste .
-            ?ste eli:title ?stt .
-            FILTER (LANG(?stt) = 'fi')
-          }
-          VALUES ?score {20}
-          VALUES ?matchType {'title'}
-          ?s a ?t .
-        } UNION {
-          (?f ?score) text:query (sfl:textLemmatized '${queryBaseForm}' 20) .
-          ?s eli:has_member ?v.
-          ?v eli:is_realized_by ?e .
-          ?e eli:is_embodied_by ?f .
-          ?e eli:is_embodied_by ?f2 .
-          ?f2 sfl:html ?txt .
-          VALUES ?matchType {'content'}
-          ?s a ?t .
-          FILTER (?t IN ( sfl:Subsection, sfl:Section ) )
-          OPTIONAL { ?e eli:title ?title } .
-          ?v eli:is_part_of+ ?st .
-          ?st eli:is_member_of ?stw .
-          ?stw a sfl:Statute .
-          FILTER NOT EXISTS {
-            ?stw eli:in_force eli:InForce-notInForce .
-          }
-          ?st eli:is_realized_by ?ste .
-          ?ste eli:title ?stt .
-          FILTER (LANG(?stt) = 'fi')
-        } UNION {
-          (?f ?score) text:query (sfl:text '${query}' 20) .
-          ?s eli:has_member ?v.
-          ?v eli:is_realized_by ?e .
-          ?e eli:is_embodied_by ?f .
-          ?e eli:is_embodied_by ?f2 .
-          ?f2 sfl:html ?txt .
-          VALUES ?matchType {'content'}
-          ?s a ?t .
-          FILTER (?t IN ( sfl:Subsection, sfl:Section ) )
-          OPTIONAL { ?e eli:title ?title } .
-          ?v eli:is_part_of+ ?st .
-          ?st eli:is_member_of ?stw .
-          ?stw a sfl:Statute .
-          FILTER NOT EXISTS {
-            ?stw eli:in_force eli:InForce-notInForce .
-          }
-          ?st eli:is_realized_by ?ste .
-          ?ste eli:title ?stt .
-          FILTER (LANG(?stt) = 'fi')
-        } UNION {
-          ?c a skos:Concept .
-          ?c skos:prefLabel ?l .
-          (?c ?score) text:query (skos:prefLabel '${query}' 20) .
-          VALUES ?matchType {'keyword'}
-          ?s eli:has_member ?v.
-          ?v eli:is_about ?c .
-          ?v eli:is_realized_by ?e .
-          ?e eli:is_embodied_by ?f .
-          ?e eli:is_embodied_by ?f2 .
-          ?f2 sfl:html ?txt .
-          VALUES ?matchType {'content'}
-          ?s a ?t .
-          FILTER (?t IN ( sfl:Subsection, sfl:Section ) )
-          OPTIONAL { ?e eli:title ?title } .
-          ?v eli:is_part_of+ ?st .
-          ?st eli:is_member_of ?stw .
-          ?stw a sfl:Statute .
-          FILTER NOT EXISTS {
-            ?stw eli:in_force eli:InForce-notInForce .
-          }
-          ?st eli:is_realized_by ?ste .
-          ?ste eli:title ?stt .
-          FILTER (LANG(?stt) = 'fi')
-        }
-      } GROUP BY ?s ?v ?st ?stt ?c ?l ?t ?title ?txt ?score ?matchType ORDER BY DESC(?score) LIMIT 20
-    `;
-  },
 
   findOne: (params) => {
     const sectionOfALaw = params.sectionOfALaw ? params.sectionOfALaw.replace(/\//gi, '\\/') : '';
@@ -150,7 +21,7 @@ const statuteQuery = {
     const treeFilter = params.hasOwnProperty('tree') ? 'FILTER NOT EXISTS { ?s eli:has_part _:b . }' : '';
     const contentProperty = sfl.getPropertyByFormat(params.format || 'text');
 
-    return `SELECT * ${fromGraph} WHERE {
+    return `SELECT DISTINCT * ${fromGraph} WHERE {
       {
         {
           SELECT DISTINCT ?statute (MAX(COALESCE(?vd, "")) AS ?versionDate) WHERE {
@@ -162,6 +33,7 @@ const statuteQuery = {
             }
           } GROUP BY ?statute ${limit} ${offset}
         }
+        FILTER(BOUND(?statute))
         # Matching consolidated version
         OPTIONAL {
           ?statute eli:has_member ?statuteVersion .
@@ -195,7 +67,82 @@ const statuteQuery = {
         ?s ?p ?o.
       }
     }`;
+  },
+
+  findMany: (params) => {
+    const fromGraph = params.free ? `FROM ${FREE_GRAPH}` : '';
+    const yearFilter = params.year ? `FILTER STRENDS(?idLocal, "${params.year}")` : '';
+    const original = params.version === 'alkup' ? '?statuteVersion eli:version sfl:Original .' : '';
+    const lang = eli.getLangResource(params.lang || 'fi');
+
+    const limit = params.year ? '' : `LIMIT ${params.limit || DEFAULT_LIMIT}`;
+    const offset = params.offset ? `OFFSET ${params.offset}` : '';
+
+    const refinedParams = {
+      query: params.query,
+      queryBaseForm: params.queryBaseForm,
+      fromGraph: fromGraph,
+      yearFilter: yearFilter,
+      lang: lang,
+      original: original
+    };
+
+    const resultset = params.query ? getFindManyByQueryResultset(refinedParams) : getFindManyResultset(refinedParams);
+
+    return `SELECT DISTINCT * ${fromGraph} WHERE {
+      {
+        SELECT DISTINCT ?statute ?score {
+          ${resultset}
+          BIND(xsd:integer(REPLACE(?idLocal, ".*/([0-9]+).?$", "$1")) AS ?year)
+          BIND(xsd:integer(REPLACE(?idLocal, "[^0-9]+[0-9]+", "")) AS ?number)
+          BIND(REPLACE(?idLocal, "[^A-Z]", "") AS ?letter)
+        } ORDER BY DESC(?score) ?year ?number ?letter ${limit} ${offset}
+      }
+      FILTER(BOUND(?statute))
+      ?statute eli:id_local ?idLocal .
+      ?statute sfl:statuteType ?statuteType .
+      ?statute eli:has_member ?statuteVersion .
+      ?statuteVersion eli:is_realized_by ?expression .
+      ?statuteVersion a ?statuteVersionType .
+      ?expression eli:language ${lang} .
+      ?expression eli:title ?title .
+      ?expression a ?expressionType .
+    }`;
   }
 };
+
+function getFindManyResultset(params) {
+  return `
+    ${params.original}
+    ?statute eli:id_local ?idLocal .
+    ${params.yearFilter}
+    ?statute a sfl:Statute .
+    ?statute eli:has_member ?statuteVersion .
+    ?statuteVersion eli:is_realized_by ?expression .
+    ?expression eli:language ${params.lang} .
+    ?expression eli:title ?title .
+  `;
+}
+
+function getFindManyByQueryResultset(params) {
+  const sanitize = (qry) => qry.replace(/[^a-zA-ZäöåÄÖÅ0-9*"\s]/gi, '').toLowerCase();
+  let query = sanitize(params.query);
+  if (params.queryBaseForm) {
+    query += ' ' + sanitize(params.queryBaseForm);
+  }
+
+  return `
+    (?format ?score) text:query (sfl:text '${query}') .
+    ?expression eli:is_embodied_by ?format .
+    ?expression eli:language ${params.lang} .
+    ?expression eli:title ?title .
+    ?statuteVersion eli:is_realized_by ?expression .
+    ${params.original}
+    ?statute eli:has_member ?statuteVersion .
+    ?statute a sfl:Statute .
+    ?statute eli:id_local ?idLocal .
+    ${params.yearFilter}
+    ?expression eli:language ${params.lang} .`;
+}
 
 export default statuteQuery;
