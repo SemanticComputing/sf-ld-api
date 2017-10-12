c124aac96596fa44038106abedcb3c90b193d8dc
Arttu Oksanen <arttu.oksanen@aalto.fi>
2017-10-12 23:14:28 +0300

    Fix statute / section of law local id in statute list

--- src/shared/components/StatuteListItem.js
+++ src/shared/components/StatuteListItem.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router';
 import { object } from 'prop-types';
 import prefix from '../lib/prefix';
 import LegislationJsonLd from '../lib/LegislationJsonLd';
+import eli from '../lib/eli';
 
 export default function StatuteListItem(props) {
   return (
     <li className="statute-list-item">
-      <Link to={prefix.lengthen(props.statute['@id'])}>{props.statute.idLocal} - {new LegislationJsonLd().getStatuteTitle(props.statute)}</Link>
+      <Link to={prefix.lengthen(props.statute['@id'])}>{eli.getStatuteLocalId(props.statute['@id'])} {eli.getSectionOfALawLocalId(props.statute['@id'])} - {new LegislationJsonLd().getStatuteTitle(props.statute)}</Link>
     </li>
   );
 }
